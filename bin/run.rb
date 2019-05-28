4542e92a03b29e6126d585f18dea3e266989c608
Fabian <fabrofabian@gmail.com>
2019-05-28 16:33:07 -0700

    added welcome message

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
 require_relative '../config/environment'
 
-
+welcome
 
 puts "HELLO WORLD"
---
+++ lib/welcome.rb
@@ -0,0 +1,30 @@
+def welcome
+    puts "Welcome to the Lyric Builder App!!!"
+
+    puts "Are you a Returning user? y/n"
+    input = gets.chomp
+
+    if input == 'y'
+      print "Login name: "
+      login_input = gets.chomp
+      returning_user = User.find_by(name: login_input)
+      puts "Hello #{returning_user.name}!"
+    elsif input == 'n'
+      print "Enter your name: "
+      name_input = gets.chomp
+      #binding.pry
+      User.create(name: name_input)
+    end
+
+end
+
+
+
+# If No user Exists in database
+# 	Hello New User
+#
+# 	Enter you name:
+# Else
+# 	Returning user?
+# 	Enter your name:
+# End
